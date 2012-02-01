dd28363ac07cdbda4b76048c48a3da29b9a03931
Joshua Ruihley <ruihley@gmail.com>
2012-02-01 01:02:30 +0000

    server successfully outputting command sent from client

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt()
+@csrf_exempt
 def batch(request):
-    return HttpResponse("dummy response")
+    return HttpResponse(request.POST['items'])
