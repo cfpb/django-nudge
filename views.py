36d1f9e62ebc1a1a139f5fa0e6071ff2a6ee866d
Joshua Ruihley <ruihley@gmail.com>
2012-02-02 19:20:03 +0000

    added web service to receive and process add/update commands in batch

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from server import process_batch
+
 @csrf_exempt
 def batch(request):
-    return HttpResponse(request.POST['items'])
+    result = process_batch(request.POST)
+    return HttpResponse(result)
