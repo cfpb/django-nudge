a9138b306a5a678dd139930d73ca77e8f5bec64e
Joshua Ruihley <ruihley@gmail.com>
2012-02-04 16:47:16 +0000

    updated server methods to deal with encryption

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
+import binascii
+import pickle
 import json
+
+from Crypto.Cipher import AES
 
 from django.db import models
 from django.utils import importlib
 from utils import convert_keys_to_string
 
+from nudge.models import Setting
+
+"""
+server.py 
+
+commands received from nudge client
+"""
+
+SETTINGS = Setting.objects.get(pk=1)
 
 def get_model(model_str):
     app_name = model_str.split('.')[0]
@@ -15,6 +28,12 @@ def get_model(model_str):
 def valid_batch(batch_info):
     is_valid = ('id' in batch_info) and ('title' in batch_info) and ('items' in batch_info)
     return is_valid
+    
+def decrypt(key, ciphertext):
+    ciphertext = binascii.unhexlify(ciphertext)
+    decobj = AES.new(key, AES.MODE_ECB)
+    plaintext = decobj.decrypt(ciphertext)
+    return plaintext
     
 def process_item(item):
     if item['fields']['type'] < 2:
@@ -32,10 +51,11 @@ def process_item(item):
         return False
     
 def process_batch(batch_info):
+    key = SETTINGS.local_key
+    batch_info = pickle.loads(decrypt(key, batch_info))
     if valid_batch(batch_info):
         items = json.loads(batch_info['items'])
         success = True
         for item in items:
             success = success and process_item(item)
-        
     return success
